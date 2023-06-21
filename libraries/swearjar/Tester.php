81ea4f16b1533fa7caf0fd96c4b6e74ba07079ce
AdamHebby <adamhebden56@gmail.com>
2023-06-22 00:45:46 +0100

    Add better type hinting and general clean up

--- libraries/swearjar/Tester.php
+++ libraries/swearjar/Tester.php
@@ -10,15 +10,12 @@ use \Symfony\Component\Yaml\Yaml;
  * @package swearjar
  */
 class Tester {
-
-	protected $_matchers = array();
+	protected array $matchers = array();
 
 	/**
 	 * Constructor
-	 *
-	 * @param string $file
 	 */
-	public function __construct($file = null) {
+	public function __construct(?string $file = null) {
 		if ($file === null) {
 			$file = __DIR__ . '/config/en.yml';
 		}
@@ -28,12 +25,9 @@ class Tester {
 
 	/**
 	 * Loads a YAML file containing rules for matching profanity.
-	 *
-	 * @param string $path  YAML ruleset file path
-	 * @return void
 	 */
-	public function loadFile($path) {
-		$this->_matchers = Yaml::parse(file_get_contents($path));
+	public function loadFile(string $path): void {
+		$this->matchers = Yaml::parse(file_get_contents($path));
 	}
 
 	/**
@@ -41,35 +35,32 @@ class Tester {
 	 * each instance of profanity.
 	 *
 	 * The signature of `$callback` is:
-	 * 
+	 *
 	 *    function ($word, $index, $types) { ... }
-	 * 
+	 *
 	 * Where `$word` is the possible profane word, `$index` is the offset of the word
 	 * in the text, and `$types` is a list of tags for the word.
-	 * 
-	 * @param string $text
-	 * @param Closure $callback
-	 * @return void
 	 */
-	public function scan($text, \Closure $callback) {
+	public function scan(string $text, \Closure $callback): void {
 		preg_match_all('/\w+/u', $text, $matches, PREG_OFFSET_CAPTURE);
 
 		foreach ($matches[0] as $match) {
-			list($word, $index) = $match;
+			[$word, $index] = $match;
 
 			$key = mb_strtolower($word);
 
-			if (array_key_exists($key, $this->_matchers['simple'])) {
-				if ($callback->__invoke($word, $index, $this->_matchers['simple'][$key]) === false) {
+			if (array_key_exists($key, $this->matchers['simple'])) {
+				if ($callback->__invoke($word, $index, $this->matchers['simple'][$key]) === false) {
 					return;
 				}
 			}
 		}
 
-		foreach ($this->_matchers['regex'] as $regex => $types) {
+		foreach ($this->matchers['regex'] as $regex => $types) {
 			preg_match_all('/' . $regex . '/i', $text, $matches, PREG_OFFSET_CAPTURE);
 			foreach ($matches[0] as $match) {
-				list($word, $index) = $match;
+				[$word, $index] = $match;
+
 				if ($callback->__invoke($word, $index, $types) === false) {
 					return;
 				}
@@ -79,11 +70,8 @@ class Tester {
 
 	/**
 	 * Returns true if `$text` contains profanity
-	 *
-	 * @param string $text
-	 * @return boolean
 	 */
-	public function profane($text) {
+	public function profane(string $text): bool {
 		$profane = false;
 
 		$this->scan($text, function($word, $index, $types) use (&$profane) {
@@ -97,19 +85,14 @@ class Tester {
 	/**
 	 * Analyzes `$text` and generates a report of the type of profanity found.
 	 *
-	 * @param string $text
-	 * @return array
+	 * @return array<string,int>
 	 */
-	public function scorecard($text) {
-		$scorecard = array();
+	public function scorecard(string $text): array {
+		$scorecard = [];
 
 		$this->scan($text, function($word, $index, $types) use (&$scorecard) {
 			foreach ($types as $type) {
-				if (isset($scorecard[$type])) {
-					$scorecard[$type] += 1;
-				} else {
-					$scorecard[$type] = 1;
-				}
+				$scorecard[$type] = ($scorecard[$type] ?? 0) + 1;
 			}
 
 			return true;
@@ -120,12 +103,8 @@ class Tester {
 
 	/**
 	 * Scans `$text` and censors profanity.
-	 *
-	 * @param string $text
-	 * @param boolean $hint (optional)
-	 * @return string
 	 */
-	public function censor($text, $hint = false) {
+	public function censor(string $text, bool $hint = false): string {
 		$censored = $text;
 
 		$offset = $hint ? 1 : 0;
